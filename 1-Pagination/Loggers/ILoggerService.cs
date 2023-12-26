9df363b0655e888e0e7004d4f11aae03014865e3
alkanfatih <alkanfatih87@gmail.com>
2023-12-26 15:12:45 +0300

    Nlog-1

---
+++ 1-Pagination/Loggers/ILoggerService.cs
@@ -0,0 +1,10 @@
+﻿namespace _1_Pagination.Loggers
+{
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+        void LogDebug(string message);
+    }
+}
---
+++ 1-Pagination/Loggers/LoggerService.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+namespace _1_Pagination.Loggers
+{
+    public class LoggerService : ILoggerService
+    {
+        private static NLog.ILogger logger = NLog.LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message) => logger.Debug(message);
+
+        public void LogError(string message) => logger.Error(message);
+
+        public void LogInfo(string message) => logger.Info(message);
+
+        public void LogWarning(string message) => logger.Warn(message);
+    }
+}
