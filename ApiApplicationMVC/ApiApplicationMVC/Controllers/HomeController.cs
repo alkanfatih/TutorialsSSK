863b86f115b9f64f023528f2bf2fafb82f45c3fe
alkanfatih <alkanfatih87@gmail.com>
2023-12-28 10:36:42 +0300

    Api Uygulama

---
+++ ApiApplicationMVC/ApiApplicationMVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ApiApplicationMVC.Models;
+using ApiApplicationMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ApiApplicationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApiService _apiService;
+
+        public HomeController(ILogger<HomeController> logger, ApiService apiService)
+        {
+            _logger = logger;
+            _apiService = apiService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetData()
+        {
+            List<Person> apiData = await _apiService.GetApiDataAsync();
+            if (apiData != null)
+            {
+                return View(apiData);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ApiApplicationMVC/ApiApplicationMVC/Services/ApiService.cs
@@ -0,0 +1,39 @@
+﻿using ApiApplicationMVC.Models;
+using System.Text.Json;
+
+namespace ApiApplicationMVC.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Person>> GetApiDataAsync()
+        {
+            string apiUrl = "https://localhost:7073/api/Values/GetAllPersons";
+
+            //istek
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, apiUrl);
+            request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWxrYW5AbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTcwMzc1MTk2MywiaXNzIjoiYnNzdG9yZWFwaSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJ9.a8vtLugKahOeG7oATm1ujthMU9qpNfAuZrqh6RelPFE");
+
+            //yanıt
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                List<Person> person = JsonSerializer.Deserialize<List<Person>>(responseData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                return person;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
